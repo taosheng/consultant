4d60b507122d959006ee5f868c41ba1240aa2273
root <root@tsu.client.tw.trendnet.org>
2013-01-19 09:26:01 +0800

    add P4 change log script and also modify the format of parser

--- application/python/P4ParserMap.py
+++ application/python/P4ParserMap.py
@@ -13,31 +13,40 @@ def resetDict(oneChange):
     oneChange['desc'] = ''
     return 
 
+def read_input(file):
+    for line in file:
+        # split the line into words
+        yield line.split()
+
 def main(separator='\t'):
+
     # input comes from STDIN (standard input)
 #    data = read_input(sys.stdin)
     
+#    print data
     oneChange = {'id':'','action':'','time':'','submitter':'','reviewer':'','files':[],'desc':''}
     lineCnt = 0
     for line in sys.stdin:
         lineCnt +=1
         line = line.strip()
-        if re.match('.*#.* change .*', line) is not None:
-            print str(oneChange)
+        if re.match('Change .*', line) is not None:
+            if oneChange['reviewer'] == '':
+                print str(oneChange)
+         
             resetDict(oneChange)
             parts = line.split(" ")
             oneChange['id'] = parts[3]
             oneChange['action'] = parts[4]
-            oneChange['time'] = parts[6]
-            oneChange['submitter'] = parts[8].split("@")[0]
+            oneChange['time'] = parts[3]
+            oneChange['submitter'] = parts[5].split("@")[0]
         elif re.match('.*Reviewed by.*',line) is not None:
             reviewerLineParts = line.split("Reviewed by: ")
             if len(reviewerLineParts) > 1:
                 oneChange['reviewer'] = line.split("Reviewed by: ")[1][1:-1]
  
-        elif re.match('\.\.\. \.\.\..*',line) is not None:
-            filename = line.split(" ")[4].split("#")[0]
-            oneChange['files'].append(filename)
+      #  elif re.match('\.\.\. \.\.\..*',line) is not None:
+      #      filename = line.split(" ")[4].split("#")[0]
+      #      oneChange['files'].append(filename)
         else:
             oneChange['desc'] =  oneChange['desc'] + line
             
---
+++ application/python/p4_get_changelogs.sh
@@ -0,0 +1,14 @@
+p4user="Tao-Sheng_Chen"
+P4PASSWD=$1
+p4port="10.201.16.19:1667"
+p4client="`hostname`"
+p4flag="-c $p4client -p $p4port -u $p4user"
+
+# P4 login by autouser
+~/bin/p4 $p4flag login
+
+# Check out latest codes
+#p4 sync
+
+#~/bin/p4 $p4flag changes "@2013/01/15,@now" "//Ent/SSFB/Dev/SSFB_Server-2.1/..."
+~/bin/p4 $p4flag -L changes   "//Ent/SSFB/Dev/SSFB_Server-2.1/..."@2013/01/15,@now
